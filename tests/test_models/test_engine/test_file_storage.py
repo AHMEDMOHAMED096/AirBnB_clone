83825eb589ea9e8ba074335954aad1e29b7871ac
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-03-26 12:03:07 -0700

    added unittest for save method

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Import unittest & file_storage to create unittest & FileStorage classes."""
 import unittest
+import os
+import json
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
@@ -10,9 +12,22 @@ class TestFileStorage(unittest.TestCase):
 
     def setUp(self):
         """Method to create instance of BaseModel &
-        FileStorage before every test."""
+        FileStorage before each test."""
         self.storage = FileStorage()
         self.base_model = BaseModel()
+        self.storage.__file_path = "file.json"
+
+    def tearDown(self):
+        """Method to remove json file after each test."""
+        os.remove(self.obj.__file_path)
+
+    def test_save(self):
+        """Method to test serialization objects to json file."""
+        self.storage.save()
+        with open(self.storage.__file_path) as file:
+            data = json.load(file)
+            self.assertEqual(data, {"obj": self.storage.__objects
+                                    ["obj"].to_dict()})
 
     def test_all(self):
         """Method to test the return of the dictionary of instances."""
